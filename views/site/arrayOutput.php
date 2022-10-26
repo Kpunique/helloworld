0df262c0158ea969f371d8522196384654fad215
kpunique <kpunique@gmail.com>
2022-10-25 23:25:15 -0500

    http://localhost:8080/index.php?r=site%2Farray

---
+++ views/site/arrayOutput.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+$newArray = array("Learn", "Grow", "Code", "Fun");
+array_push($newArray, "$model->item");
+
+$count = count($newArray);
+
+$part1 = array_slice($newArray, 0, $count - 1);
+$sentence = implode(",", $part1) . ' and ' . $newArray[$count - 1];
+
+
+//echo Html::encode($sentence); 
+?>
+<p>You have entered the following information:</p>
+
+<ul>
+    <li><label>Name</label>: <?= Html::encode($model->item) ?></li>
+</ul>
+
+<p> your array value in a sentence is <?= Html::encode($sentence) ?></p>
